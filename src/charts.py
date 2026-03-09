3ee66e91fddab90982490e0f6590239fbe603b2d
sjgflaied <gfy1213@uw.edu>
2026-03-09 15:00:36 -0700

    Update charts.py

--- src/charts.py
+++ src/charts.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
 import numpy as np
-from scipy import stats
 
 def sales_overview(df, categories, selected_year, chart_type):
 
